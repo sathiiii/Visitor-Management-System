f53f39420821b8574fef29ec081f609c07bdc57c
Sathira Silva <e17331@eng.pdn.ac.lk>
2020-08-02 23:00:17 +0530

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,36 @@
-// Function to toggle visibility of an element
 function toggle_visibility(id) {
     document.getElementById(id).classList.toggle("show");
   }
 
-// Collapse all open dropdowns when user click outside
 window.onclick = function(event) {
     if (!event.target.matches(".dropbtn")) {
         var dropdowns = document.getElementsByClassName("dropdown");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            console.log(i);
             if (openDropdown.classList.contains("show"))
                 openDropdown.classList.remove("show");
         }
     }
+}
+
+window.onscroll = function(event) {  
+    var scroll = document.documentElement.scrollTop;
+    var header = document.getElementById("header");
+    if (scroll > 0) {
+        header.classList.add("active");
+    }
+    else {
+        header.classList.remove("active");
+    }
+}
+
+window.onresize = function(event) {
+    var dropdowns = document.getElementsByClassName("dropdown");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show"))
+                openDropdown.classList.remove("show");
+        }
 }
