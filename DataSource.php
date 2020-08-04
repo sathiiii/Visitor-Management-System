27119e82af5846eaa887b52cf30d87d047f1ff2c
Sathira Silva <e17331@eng.pdn.ac.lk>
2020-08-04 12:43:00 +0530

    Add files via upload

---
+++ DataSource.php
@@ -0,0 +1,129 @@
+<?php
+namespace VMS;
+
+class DataSource
+{
+    const HOST = 'localhost';
+    const USERNAME = 'root';
+    const PASSWORD = '';
+    const DATABASENAME = 'vms';
+
+    private $conn;
+
+    function __construct()
+    {
+        $this->conn = $this->getConnection();
+    }
+
+    public function getConnection()
+    {
+        $conn = new \mysqli(self::HOST, self::USERNAME, self::PASSWORD, self::DATABASENAME);
+        if (mysqli_connect_errno()) {
+            trigger_error("Problem with connecting to database.");
+        }
+        $conn->set_charset("utf8");
+        return $conn;
+    }
+
+    /**
+     * To get database results
+     * @param string $query
+     * @param string $paramType
+     * @param array $paramArray
+     * @return array
+     */
+    public function select($query, $paramType="", $paramArray=array())
+    {
+        $stmt = $this->conn->prepare($query);
+        
+        if(!empty($paramType) && !empty($paramArray)) {
+            $this->bindQueryParams($stmt, $paramType, $paramArray);
+        }
+        
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $resultset[] = $row;
+            }
+        }
+        
+        if (! empty($resultset)) {
+            return $resultset;
+        }
+    }
+    
+    /**
+     * To insert
+     * @param string $query
+     * @param string $paramType
+     * @param array $paramArray
+     * @return int
+     */
+    public function insert($query, $paramType, $paramArray)
+    {
+        print $query;
+        $stmt = $this->conn->prepare($query);
+        $this->bindQueryParams($stmt, $paramType, $paramArray);
+        $stmt->execute();
+        $insertId = $stmt->insert_id;
+        return $insertId;
+    }
+    
+    /**
+     * To execute query
+     * @param string $query
+     * @param string $paramType
+     * @param array $paramArray
+     */
+    public function execute($query, $paramType="", $paramArray=array())
+    {
+        $stmt = $this->conn->prepare($query);
+        
+        if(!empty($paramType) && !empty($paramArray)) {
+            $this->bindQueryParams($stmt, $paramType="", $paramArray=array());
+        }
+        $stmt->execute();
+    }
+    
+    /**
+     * 1. Prepares parameter binding
+     * 2. Bind prameters to the sql statement
+     * @param string $stmt
+     * @param string $paramType
+     * @param array $paramArray
+     */
+    public function bindQueryParams($stmt, $paramType, $paramArray=array())
+    {
+        $paramValueReference[] = & $paramType;
+        for ($i = 0; $i < count($paramArray); $i ++) {
+            $paramValueReference[] = & $paramArray[$i];
+        }
+        call_user_func_array(array(
+            $stmt,
+            'bind_param'
+        ), $paramValueReference);
+    }
+    
+    /**
+     * To get database results
+     * @param string $query
+     * @param string $paramType
+     * @param array $paramArray
+     * @return array
+     */
+    public function numRows($query, $paramType="", $paramArray=array())
+    {
+        $stmt = $this->conn->prepare($query);
+        
+        if(!empty($paramType) && !empty($paramArray)) {
+            $this->bindQueryParams($stmt, $paramType, $paramArray);
+        }
+        
+        $stmt->execute();
+        $stmt->store_result();
+        $recordCount = $stmt->num_rows;
+        return $recordCount;
+    }
+}
---
+++ Member.php
@@ -0,0 +1,40 @@
+<?php
+namespace VMS;
+
+use \VMS\DataSource;
+
+class Member
+{
+
+    private $dbConn;
+
+    private $ds;
+
+    function __construct()
+    {
+        require_once "DataSource.php";
+        $this->ds = new DataSource();
+    }
+
+    function getMemberById($adminId, $tablename)
+    {
+        $query = "select * FROM $tablename WHERE admin_id = ?";
+        $paramType = "i";
+        $paramArray = array($memberId);
+        $memberResult = $this->ds->select($query, $paramType, $paramArray);
+        
+        return $memberResult;
+    }
+    
+    public function processLogin($username, $password, $tablename) {
+        $passwordHash = md5($password);
+        $query = "select * FROM $tablename WHERE user_name = ? AND password = ?";
+        $paramType = "ss";
+        $paramArray = array($username, $passwordHash);
+        $memberResult = $this->ds->select($query, $paramType, $paramArray);
+        if(!empty($memberResult)) {
+            $_SESSION["userId"] = $memberResult[0]["id"];
+            return true;
+        }
+    }
+}
