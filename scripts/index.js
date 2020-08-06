167bb758e387d8b783e15d628f99f501c2256c0c
Sathira Silva <e17331@eng.pdn.ac.lk>
2020-08-06 11:40:26 +0530

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,17 @@ var username = "<?php echo $username ?>";
 
 function toggle_visibility(id) {
     document.getElementById(id).classList.toggle("show");
-  }
+}
+
+window.onload = function() {
+    if(!document.getElementById("checkstatus").checked) {
+        document.getElementById("status").innerText = "inactive";
+        document.getElementById("status").style.color = "orange";
+    }
+}
 
 window.onclick = function(event) {
-    if (!event.target.matches(".dropbtn")) {
+    if (!event.target.matches(".dropbtn") && event.target.id != "stat" && event.target.id != "status") {
         var dropdowns = document.getElementsByClassName("dropdown");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
@@ -29,10 +36,21 @@ window.onscroll = function(event) {
 
 window.onresize = function(event) {
     var dropdowns = document.getElementsByClassName("dropdown");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show"))
-                openDropdown.classList.remove("show");
-        }
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show"))
+            openDropdown.classList.remove("show");
+    }
+}
+
+function updateStatus(checkbox) {
+    if (checkbox.checked) {
+        document.getElementById("status").innerText = "active";
+        document.getElementById("status").style.color = "#2CD889";
+    }
+    else {
+        document.getElementById("status").innerText = "inactive";
+        document.getElementById("status").style.color = "orange";
+    }
 }
